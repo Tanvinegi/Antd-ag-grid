7dd93d2239972aaf80a19b7106769669c2a74807
Tanvinegi57 <tanvi.n@applify.co>
2024-04-21 16:47:53 +0530

    git init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import { Button, Input } from 'antd'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <Button type="primary">fsfsdf</Button>
+      <Input/>
+    </div>
+    </>
+  )
+}
+
+export default App
